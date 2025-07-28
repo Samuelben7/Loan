016f2d0e5c9843fd5b4a4648db5d0f7a7c971442
Samuel Benjamim <contatosamuelbenjamim@gmail.com>
2025-07-28 18:32:26 -0300

    fix the error5

--- Loan/settings.py
+++ Loan/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Loan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'loan',
@@ -92,14 +92,14 @@ WSGI_APPLICATION = 'Loan.wsgi.application'
 
 
 
-"""
-DATABASES = {
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

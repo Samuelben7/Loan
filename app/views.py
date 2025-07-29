3a617d4d9c89054ad22557bae156ff514a6e79ab
Samuel Benjamim <contatosamuelbenjamim@gmail.com>
2025-07-28 21:07:46 -0300

    add grafic9

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
+from datetime import date
 from datetime import timedelta
 from .models import ContratoEmprestimo, Parcela
 from rest_framework_simplejwt.tokens import RefreshToken
